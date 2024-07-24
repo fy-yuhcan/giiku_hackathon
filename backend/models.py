cef9c47bf6c19e686496bb71d7f0fa58e2d1b457
harei.whitewings@gmail.com <harei.whitewings@gmail.com>
2024-07-25 01:18:24 +0900

    テーブルの全てのフィールド(主キー以外)をNULL非許容に設定

--- backend/models.py
+++ backend/models.py
@@ -10,37 +10,37 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_Key=True)
-    name = Column(String)
-    password = Column(String)
+    name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
 
 
 class Food(Base):
     __tablename__ = "foods"
     id = Column(Integer, primary_Key=True)
-    name = Column(String)
-    unit = Column(String)
+    name = Column(String, nullable=False)
+    unit = Column(String, nullable=False)
 
 
 class Recipe(Base):
     __tablename__ = "recipes"
     id = Column(Integer, primary_Key=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    text = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    text = Column(String, nullable=False)
 
 
 class RecipeFood(Base):
     __tablename__ = "recipe_foods"
     id = Column(Integer, primary_Key=True)
-    food_id = Column(Integer, ForeignKey("foods.id"))
-    recipe_id = Column(Integer, ForeignKey("recipes.id"))
-    quantity = Column(Integer)
+    food_id = Column(Integer, ForeignKey("foods.id"), nullable=False)
+    recipe_id = Column(Integer, ForeignKey("recipes.id"), nullable=False)
+    quantity = Column(Integer, nullable=False)
     
 
 
 class Fridge(Base):
     __tablename__ = "fridges"
     id = Column(Integer, primary_Key=True)
-    food_id = Column(Integer, ForeignKey("foods.id"))
-    user_id = Column(Integer, ForeignKey("users.id"))
-    added_at = Column(DateTime)
-    quantity = Column(Integer)
+    food_id = Column(Integer, ForeignKey("foods.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    added_at = Column(DateTime, nullable=False)
+    quantity = Column(Integer, nullable=False)
