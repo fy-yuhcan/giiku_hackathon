586e476e9872e1c9debafd73a9fabab7e4c68f98
harei.whitewings@gmail.com <harei.whitewings@gmail.com>
2024-07-27 00:54:48 +0900

    食材を追加するデータベース操作

--- backend/crud/foods.py
+++ backend/crud/foods.py
@@ -1,6 +1,21 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.expression import *
+from sqlalchemy.ext.asynccio import AsyncSession
+from sqlalchemy import text
 from fastapi import HTTPException
+from ..database import Base
 
 #from schemas import 
 from ..models import User, Food, Recipe, RecipeFood, Fridge
+
+async def add_food(session: AsyncSession, name: str, unit: str) -> User:
+    query = text(
+        "INSERT INTO foods" +
+        "(name, unit) " +
+        "VALUES " +
+        "(:name, :unit)"
+    )
+
+    result = await session.execute(query, {"name": name, "unit": unit})
+    await session.commit()
+    return result.fetchone()
