639442a6c139b6c23f5902b32ac4523a684ae59e
kaikitano0610 <k.kitano.kai@gmail.com>
2024-07-27 01:32:50 +0900

    Foodhandlerの修正

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from '../../components/Header.js';
-
+import Header from '../components/Header.js';
+import FoodHandler from '../components/FoodHandler.js';
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-yellow-100 flex flex-col justify-between">
       <Header />
       <main className="flex flex-col items-center mt-10">
-
+        <FoodHandler />
       </main>
     </div>
   );
