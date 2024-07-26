c7cfbd475cc223b38c82ff29b2cca01fc6f410cd
harei.whitewings@gmail.com <harei.whitewings@gmail.com>
2024-07-27 02:50:25 +0900

    非同期エンジン？にした

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm.decl_api import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -16,7 +17,12 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{PO
 
 
 Engine = create_engine(SQLALCHEMY_DATABASE_URL)
+Engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=Engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=Engine, class_=AsyncSession)
 
 Base = declarative_base()
+
+async def get_session() -> AsyncSession:
+    async with SessionLocal() as session:
+        yield session
