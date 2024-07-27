3a056d10b4fcc745426e5c912fc5350742798383
suisan0731 <suisan0731@gmail.com>
2024-07-27 17:20:21 +0900

    ファイルのアップロード

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,6 @@ const Header = () => {
   return (
     <header className="bg-yellow-300 p-4 text-center">
       <div className="flex items-center justify-center">
-        <img src="/logo.png" alt="Logo" className="h-12 mr-4" />
         <div>
           <h1 className="text-3xl font-bold text-orange-500">食マネ</h1>
           <p className="text-lg text-orange-400">食材マネージャー</p>
--- frontend/src/components/Picture.tsx
+++ frontend/src/components/Picture.tsx
@@ -1,32 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSWRMutation from 'swr/mutation';
 
 export default function Picture() {
-  //写真をバックエンドに保存するフェッチ
-  async function updateImage(url:string, { arg }: {arg: {path: }}) {
+  
+  //保存する画像のパス
+  const DetectionFoodImagePath = "/storage/img/"
+
+  async function uploadImage(url:string, { arg }: {arg: {file: string}}) {
+    const formData = new FormData();
+    formData.append('upload_file', arg.file)
+    //写真をバックエンドに保存するapiにアクセス
     fetch(url, {
       method: "POST",
-      body: JSON.stringify({
-        user_id: arg.user_id,
-        foods: arg.foods
-      })
+      body: formData, 
     })
     .then(res => res.json())
   }
-  const { trigger, isMutating } = useSWRMutation("/fridge", updateImage)
-  
-  const handleSubmit = async (event) => {
-    await trigger({user_id: user.user_id, foods: 
-      ingredients.map(ingredient => {return {food_id: 0, quantity: ingredient.quantity}})
-    })
-    handlePageChange()
+
+  const { trigger, isMutating } = useSWRMutation("/fridge", uploadImage)
+
+  const handleChangeFile = async (event) => {
+    event.preventDefault();
+    await trigger({file: event.target.files[0]})
   };
 
   return (
     <div className="bg-white p-6 rounded-lg shadow-md w-3/4 max-w-lg mx-auto mt-10">
       <div className="border-2 border-gray-300 p-4 rounded-md w-full h-64 flex items-center justify-center">
-        <span className="text-gray-400">写真</span>
+        <span className="text-gray-400">写真をドロップ</span>
       </div>
+        <input type='file' onChange={(event) => handleChangeFile(event)}></input>
     </div>
     );
 }
