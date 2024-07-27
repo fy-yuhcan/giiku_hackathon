79943edcc672171e080aeb4546bca113b670f4fa
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-27 12:57:53 +0900

    レシピ表示

--- backend/routers/recipes.py
+++ backend/routers/recipes.py
@@ -14,13 +14,13 @@ router = APIRouter(
 #過去にChatGPTに提案してもらったレシピを表示
 @router.get("/", response_model=RecipeGetOut)
 async def create_recipe(user_id: int, num_recipe: int = 10):
-    #処理
+    recipes = await get_recipes(session,user_id,num_recipe)
     return 
 
 #ChatGPTにレシピの提案をしてもらう
 @router.post("/", response_model=RecipePostOut)
 async def update_recipe(recipe: RecipePostIn):
-    #処理
+    
     return 
 
 #いらないレシピを削除
