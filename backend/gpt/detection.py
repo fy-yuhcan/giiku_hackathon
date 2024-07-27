99062263f2197327fa671551351b660b84cc1404
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-28 02:39:54 +0900

    画像識別のテストを削除

--- backend/gpt/detection.py
+++ backend/gpt/detection.py
@@ -9,30 +9,57 @@ from crud.foods import get_foods
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+async def load_food_data(session: AsyncSession):
+    foods = await get_foods(session)
+    return {food.name: {"id": food.id, "unit": food.unit} for food in foods}
+
 def encode_image(file: UploadFile):
     file.file.seek(0)  # ファイルの先頭に戻る
     return base64.b64encode(file.file.read()).decode('utf-8')
 
-async def detect_food(base64_image, session: AsyncSession):
+def detect_food(base64_image, session: AsyncSession):
     prompt_message = """
     これらの画像に何の食材がそれぞれ何個またはどのくらいの量写っているかJSONのリストで出力してください。
     答えだけを出力してください。
     食材の名前は日本語にしてください。
     JSONのvalueは数値のみで、単位は別のKEYのValueとして持つようにしてください。
-    食材のadded_atが昔の物をできるだけ使ってください。
+
     例：
     [
         {
-            "food_id": 3,
             "name": "ピーマン",
             "quantity": 2,
             "unit": "個"
         },
         {
-            "food_id": 5,
             "name": "にんじん",
             "quantity": 3,
             "unit": "本"
+        },
+        {
+            "name": "トマト",
+            "quantity": 7,
+            "unit": "個"
+        },
+        {
+            "name": "ブロッコリー",
+            "quantity": 1,
+            "unit": "個"
+        },
+        {
+            "name": "かぼちゃ",
+            "quantity": 1,
+            "unit": "個"
+        },
+        {
+            "name": "ほうれん草",
+            "quantity": 1,
+            "unit": "束"
+        },
+        {
+            "name": "薄力小麦粉",
+            "quantity": 750,
+            "unit": "g"
         }
     ]
     """
@@ -71,16 +98,17 @@ async def detect_food(base64_image, session: AsyncSession):
     detected_foods = detected_foods.strip("```json\n").strip("\n```")
     detected_foods = json.loads(detected_foods)
 
-    foods = await get_foods(session)
+    # 食材データを取得
+    food_data = asyncio.run(load_food_data(session))
 
-    food_dict = {food.name: {"id": food.id, "unit": food.unit} for food in foods}
-
+    # 食材情報にIDと単位を追加
     for item in detected_foods:
         food_name = item["name"]
-        if food_name in food_dict:
-            item["food_id"] = food_dict[food_name]["id"]
-            item["unit"] = food_dict[food_name]["unit"]
+        if food_name in food_data:
+            item["food_id"] = food_data[food_name]["id"]
+            item["unit"] = food_data[food_name]["unit"]
 
     return detected_foods
+
 
 
