564966f2399d6a065cdd264892da95b6e0a8a4a2
hareiharei <harei.whitewings@gmail.com>
2024-07-28 05:04:07 +0900

    いろいろprintしてみた

--- backend/crud/recipefood.py
+++ backend/crud/recipefood.py
@@ -16,10 +16,11 @@ async def add_recipe_food(session: AsyncSession, food_id: int, recipe_id: int, q
         "VALUES " +
         "(:food_id, :recipe_id, :quantity)"
     )
-    await session.execute(query, {"food_id": food_id, "recipe_id": recipe_id, "quantity": quantity})
+    result = await session.execute(query, {"food_id": food_id, "recipe_id": recipe_id, "quantity": quantity})
+    print("add_recipe_food", result)
     await session.commit()
 
-# recipe_idからそのレシピで使う全ての食材を取得
+# recipe_idからそのレシピで使う全ての食材を取得     # これのAPI実装する？
 async def get_recipe_foods(session: AsyncSession, recipe_id: int):
     query = text(
         "SELECT rf.food_id, f.name, rf.quantity, f.unit" +
