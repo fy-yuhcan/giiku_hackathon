35d4a5c8d79c5e77695e3c54c8c0eda87d438dd0
hareiharei <harei.whitewings@gmail.com>
2024-07-27 23:47:37 +0900

    レシピ提案のAPI　途中まで

--- backend/routers/recipes.py
+++ backend/routers/recipes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_session
-from schemas import RecipeGetOut, RecipePostOut, RecipeRequest, RecipeModel, RecipeSuggestion, StorageWithFoodInfo
+from schemas import RecipeGetOut, RecipePostOut, RecipeRequest, RecipeModel, RecipeSuggestion, StorageWithFoodInfo, RecipeCreate
 from crud.recipes import add_recipe, get_recipes, delete_recipe
 from crud.storages import get_storage
 from gpt.generate_recipe import generate_recipe
@@ -24,25 +24,39 @@ async def get_recipes_router(user_id: int, num_recipe: int = 10, session: AsyncS
 @router.post("/", response_model=None)      # あとでスキーマ定義する！
 async def create_recipe_router(recipe_request: RecipeRequest, session: AsyncSession = Depends(get_session)):
     try:
-        user_id = recipe_request['user_id']
-        num_servings = recipe_request['num_servings']
-        uses_storages_only = recipe_request['uses_storages_only']
-        comment = recipe_request['comment']
+        user_id = recipe_request.user_id
+        num_servings = recipe_request.num_servings
+        uses_storages_only = recipe_request.uses_storages_only
+        comment = recipe_request.comment
 
         # 食材リストを作成
-        ingredients: list[StorageWithFoodInfo] = await get_storage(session, recipe_request.user_id)
+        ingredients: list[StorageWithFoodInfo] = await get_storage(session, user_id)
 
         # ChatGPTにレシピ提案をリクエストする
         suggestion: RecipeSuggestion = await generate_recipe(ingredients, num_servings, uses_storages_only, comment)
 
         # レシピをデータベースに保存する処理
-        db_recipe: RecipeModel = await add_recipe(session, recipe_request)      # 直す！
+        db_recipe: RecipeModel = await add_recipe(session, 
+                                                  RecipeCreate(
+                                                      title=suggestion.title,
+                                                      content=suggestion.content,
+                                                      user_id=user_id
+                                                  ))
+        
+        # RecipeFoodsテーブルに追加する処理
 
         # レスポンスの生成
-        return RecipePostOut(id=db_recipe.id, title=db_recipe.title, foods=suggestion["foods"], content=suggestion["content"])  # 直す！
+        return RecipePostOut(
+            id=db_recipe.id, 
+            title=db_recipe.title, 
+            foods=suggestion["foods"], 
+            content=suggestion["content"]
+        )  # 直す！
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+# レシピ　作ったボタンの処理
 
 # いらないレシピを削除
 @router.delete("/{recipe_id}")
