d6484cff090684c302444da225319479a4c5b7ce
hareiharei <harei.whitewings@gmail.com>
2024-07-28 11:38:02 +0900

    RecipeFoodsテーブルからrecipe_idが一致するものを取得するDB操作を追加。返り値の型定義。

--- backend/crud/recipefood.py
+++ backend/crud/recipefood.py
@@ -5,11 +5,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 from fastapi import HTTPException
 
-from schemas import RecipeFoodCreate, RecipeFoodModel, FoodInRecipe
+from schemas import RecipeFoodCreate, RecipeFoodModel, FoodInRecipe, RecipeFoodBase
 from models import User, Food, Recipe, RecipeFood, Storage
 
 # レシピに一つの食材を追加
-async def add_recipe_food(session: AsyncSession, food_id: int, recipe_id: int, quantity: int):
+async def add_recipe_food(session: AsyncSession, food_id: int, recipe_id: int, quantity: int) -> None:
     query = text(
         "INSERT INTO recipe_foods" +
         "(food_id, recipe_id, quantity) " +
@@ -21,13 +21,23 @@ async def add_recipe_food(session: AsyncSession, food_id: int, recipe_id: int, q
     await session.commit()
 
 # recipe_idからそのレシピで使う全ての食材を取得     # これのAPI実装する？
-async def get_recipe_foods(session: AsyncSession, recipe_id: int):
+async def get_recipe_foods_info(session: AsyncSession, recipe_id: int) -> list[FoodInRecipe]:
     query = text(
         "SELECT rf.food_id, f.name, rf.quantity, f.unit" +
         "FROM recipe_foods AS rf " +
         "LEFT JOIN foods AS f " +
         "ON rf.food_id = f.id " +
         "WHERE rf.recipe_id = :recipe_id"
+    )
+    result = await session.execute(query, {"recipe_id": recipe_id})
+    recipe_foods = result.fetchall()
+    return recipe_foods
+
+async def get_recipe_foods(session: AsyncSession, recipe_id: int) -> list[RecipeFoodBase]:
+    query = text(
+        "SELECT food_id, quantity " +
+        "FROM recipe_foods " +
+        "WHERE recipe_id = :recipe_id"
     )
     result = await session.execute(query, {"recipe_id": recipe_id})
     recipe_foods = result.fetchall()
